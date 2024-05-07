2b4ad112f190e43722f7d9edcdc8c421501f2293
Ross Light <ross@zombiezen.com>
2024-05-07 13:58:18 -0700

    Add Lua extension and type definitions

---
+++ zb_defs.lua
@@ -0,0 +1,18 @@
+---@meta
+
+---@class derivation: userdata
+---@field name string
+---@field system string
+---@field builder string
+---@field args string[]
+---@field drvPath string
+---@field out string
+---@field [string] string|number|boolean|(string|number|boolean)[]
+
+---@param args { name: string, system: string, builder: string, args: string[], [string]: string|number|boolean|(string|number|boolean)[] }
+---@return derivation
+function derivation(args) end
+
+--- @param p (string|{path: string, name: string?})
+--- @return string
+function path(p) end
