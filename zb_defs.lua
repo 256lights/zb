13ed1ee762c123f0c9e8702c803d6475dd2a3e40
Ross Light <ross@zombiezen.com>
2024-05-07 21:40:32 -0700

    Add derivation to permitted types for extra fields on derivation

--- zb_defs.lua
+++ zb_defs.lua
@@ -7,7 +7,7 @@
 ---@field args string[]
 ---@field drvPath string
 ---@field out string
----@field [string] string|number|boolean|(string|number|boolean)[]
+---@field [string] string|number|boolean|derivation|(string|number|boolean|derivation)[]
 ---@operator concat:string
 
 ---@param args { name: string, system: string, builder: string, args: string[], [string]: string|number|boolean|(string|number|boolean)[] }
