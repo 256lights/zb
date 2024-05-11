6a01f95870ec2f0e148605e5e7feef975a1f2315
Ross Light <ross@zombiezen.com>
2024-05-11 14:47:29 -0700

    WIP tcc
    Mes is now built correctly such that it includes its modules.
    The tcc build runs the first couple `mes` invocations,
    but seems to hang forever on compiling crt1.c with the built tcc.

--- demo/bootstrap.lua
+++ demo/bootstrap.lua
@@ -1,10 +1,10 @@
-local stage0 = dofile("stage0-posix/x86_64-linux.lua")
+local stage0 <const> = dofile("stage0-posix/x86_64-linux.lua")
 
-local boot = {}
+local boot <const> = {}
 
-local gnuMirrors = {
-  "https://mirrors.kernel.org/gnu/";
-  "https://ftp.gnu.org/gnu/";
+local gnuMirrors <const> = {
+  "https://mirrors.kernel.org/gnu/",
+  "https://ftp.gnu.org/gnu/",
 }
 
 ---@param args {path: string, hash: string}
@@ -15,13 +15,29 @@ local function fetchGNU(args)
   })
 end
 
+---Construct a binary search path (such as `$PATH`)
+---containing the binaries for a set of packages.
+---@param pkgs (string|derivation)[]
+---@return string # colon-separated paths
+local function mkBinPath(pkgs)
+  local parts = {}
+  for i, x in ipairs(pkgs) do
+    if i > 1 then
+      parts[#parts + 1] = ":"
+    end
+    parts[#parts + 1] = tostring(x)
+    parts[#parts + 1] = "/bin"
+  end
+  return table.concat(parts)
+end
+
 ---@param args table
 local function kaemDerivation(args)
   local actualArgs = {
     system = "x86_64-linux";
 
     OPERATING_SYSTEM = "Linux";
-    ARCH = "x86";
+    ARCH = "amd64";
   }
   for k, v in pairs(args) do
     if k ~= "script" then
@@ -29,7 +45,7 @@ local function kaemDerivation(args)
     end
   end
   actualArgs.builder = stage0.stage0.."/bin/kaem"
-  actualArgs.args = { "-f"; toFile(args.name.."-builder.kaem", args.script) }
+  actualArgs.args = { "-f", toFile(args.name.."-builder.kaem", args.script) }
   return derivation(actualArgs)
 end
 
@@ -56,9 +72,9 @@ end
 
 -- simple-patch
 do
-  local pname = "simple-patch"
-  local version = "1.0"
-  local step = path {
+  local pname <const> = "simple-patch"
+  local version <const> = "1.0"
+  local step <const> = path {
     name = "live-bootstrap-steps-simple-patch-1.0";
     path = "live-bootstrap/steps/simple-patch-1.0";
   }
@@ -82,13 +98,14 @@ do
 end
 
 -- mes
+local mes_version = "0.26"
+local mes_tarball = fetchGNU {
+  path = "mes/mes-"..mes_version..".tar.gz";
+  hash = "sha256:0f2210ad5896249466a0fc9a509e86c9a16db2b722741c6dfb5e8f7b33e385d4";
+}
 do
   local pname = "mes"
-  local version = "0.26"
-  local mes_tarball = fetchGNU {
-    path = "mes/mes-"..version..".tar.gz";
-    hash = "sha256:0f2210ad5896249466a0fc9a509e86c9a16db2b722741c6dfb5e8f7b33e385d4";
-  }
+  local version = mes_version
   local nyacc_tarball = fetchurl {
     url = "https://archive.org/download/live-bootstrap-sources/nyacc-1.00.2-lb1.tar.gz";
     hash = "sha256:708c943f89c972910e9544ee077771acbd0a2c0fc6d33496fe158264ddb65327";
@@ -131,11 +148,73 @@ do
       cd ${SRCDIR}/${MES_PKG}\n\z
       mkdir files\n\z
       "..mkStepDir(step, {
-      "pass1.kaem";
-      "mes-0.26.x86.checksums";
-      "files/config.h";
+      "pass1.kaem",
+      "mes-0.26.x86.checksums",
+      "files/config.h",
     }).."\z
       exec kaem -f pass1.kaem\n";
+  }
+end
+
+local guile_load_path <const> = table.concat({
+  boot.mes.."/share/mes/module",
+  boot.mes.."/share/nyacc/module",
+}, ":")
+
+-- tcc-0.9.26
+do
+  local pname = "tcc"
+  local version = "0.9.26-1147-gee75a10c"
+  local tcc_tarball = fetchurl {
+    url = "https://lilypond.org/janneke/tcc/tcc-0.9.26-1147-gee75a10c.tar.gz";
+    hash = "sha256:6b8cbd0a5fed0636d4f0f763a603247bc1935e206e1cc5bda6a2818bab6e819f";
+  }
+  local step = path {
+    name = "live-bootstrap-steps-tcc-0.9.26";
+    path = "live-bootstrap/steps/tcc-0.9.26";
+  }
+
+  boot.tcc_0_9_26 = kaemDerivation {
+    name = pname.."-"..version;
+    pname = pname;
+    version = version;
+
+    PATH = mkBinPath { stage0.stage0, boot.simple_patch, boot.mes };
+    M2LIBC_PATH = stage0.M2libc;
+    MES_PKG = "mes-0.26";
+    MES_PREFIX = boot.mes;
+    GUILE_LOAD_PATH = guile_load_path;
+
+    mes_tarball = mes_tarball;
+    tcc_tarball = tcc_tarball;
+
+    script = "\z
+      PREFIX=${out}\n\z
+      BINDIR=${PREFIX}/bin\n\z
+      LIBDIR=${PREFIX}/lib/mes\n\z
+      INCDIR=${MES_PREFIX}/include/mes\n\z
+      PATH=${BINDIR}:${PATH}\n\z
+      \z
+      mkdir ${PREFIX} ${BINDIR} ${PREFIX}/lib ${LIBDIR} ${PREFIX}/include\n\z
+      \z
+      DISTFILES=${TEMPDIR}/distfiles\n\z
+      mkdir ${DISTFILES}\n\z
+      cp ${tcc_tarball} ${DISTFILES}/tcc-0.9.26.tar.gz\n\z
+      cp ${mes_tarball} ${DISTFILES}/${MES_PKG}.tar.gz\n\z
+      \z
+      SRCDIR=${TEMPDIR}/src\n\z
+      mkdir ${SRCDIR} ${SRCDIR}/tcc-0.9.26\n\z
+      cd ${SRCDIR}/tcc-0.9.26\n\z
+      mkdir simple-patches\n\z
+      "..mkStepDir(step, {
+      "pass1.kaem",
+      "tcc-0.9.26.x86.checksums",
+      "simple-patches/addback-fileopen.after",
+      "simple-patches/addback-fileopen.before",
+      "simple-patches/remove-fileopen.after",
+      "simple-patches/remove-fileopen.before",
+    }).."\z
+        exec kaem -f pass1.kaem\n";
   }
 end
 
