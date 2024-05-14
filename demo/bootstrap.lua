65efa5518a726051a8f1502166cc2d3bc96f9b28
Ross Light <ross@zombiezen.com>
2024-05-14 09:24:12 -0700

    Build automake 1.8.5

--- demo/bootstrap.lua
+++ demo/bootstrap.lua
@@ -1746,4 +1746,39 @@ boot.autoconf["2.59"] = bashStep {
   };
 }
 
+boot.automake["1.8.5"] = bashStep {
+  pname = "automake";
+  version = "1.8.5";
+
+  PATH = mkBinPath {
+    boot.automake["1.7.8"],
+    boot.autoconf["2.59"],
+    boot.perl["5.6.2"],
+    boot.gawk["3.0.4"],
+    boot.diffutils["2.7"],
+    boot.grep["2.4"],
+    boot.bison["3.4.1"],
+    boot.flex["2.6.4"],
+    boot.m4["1.4.7"],
+    boot.tcc["0.9.27-pass4"],
+    boot.bash["2.05b-pass1"],
+    boot.coreutils["6.10"],
+    boot.coreutils["5.0-pass2"],
+    boot.sed["4.0.9-pass1"],
+    boot.tar["1.12"],
+    boot.gzip["1.2.4"],
+    boot.bzip2.pass2,
+    boot.patch["2.5.9"],
+    boot.make["3.82-pass1"],
+    stage0.stage0,
+  };
+
+  tarballs = {
+    fetchGNU {
+      path = "automake/automake-1.8.5.tar.bz2";
+      hash = "sha256:84c93aaa3c3651a9e7474b721b0e6788318592509e7de604bafe4ea8049dc410";
+    },
+  };
+}
+
 return boot
