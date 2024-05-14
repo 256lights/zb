ae8f9f24cdc02517720659419d0e2c08dbbdb06a
Ross Light <ross@zombiezen.com>
2024-05-14 07:59:29 -0700

    Build automake 1.7

--- demo/bootstrap.lua
+++ demo/bootstrap.lua
@@ -1539,4 +1539,39 @@ boot.autoconf["2.53"] = bashStep {
   };
 }
 
+boot.automake["1.7"] = bashStep {
+  pname = "automake";
+  version = "1.7";
+
+  PATH = mkBinPath {
+    boot.automake["1.6.2"],
+    boot.autoconf["2.53"],
+    boot.perl["5.6.2"],
+    boot.gawk["3.0.4"],
+    boot.diffutils["2.7"],
+    boot.grep["2.4"],
+    boot.bison["3.4.1"],
+    boot.flex["2.6.4"],
+    boot.m4["1.4.7"],
+    boot.tcc["0.9.27-pass4"],
+    boot.bash["2.05b-pass1"],
+    boot.coreutils["6.10"],
+    boot.coreutils["5.0-pass2"],
+    boot.sed["4.0.9-pass1"],
+    boot.tar["1.12"],
+    boot.gzip["1.2.4"],
+    boot.bzip2.pass2,
+    boot.patch["2.5.9"],
+    boot.make["3.82-pass1"],
+    stage0.stage0,
+  };
+
+  tarballs = {
+    fetchGNU {
+      path = "automake/automake-1.7.tar.bz2";
+      hash = "sha256:6633ee1202375e3c8798a92e1b7f46894f78d541aeea7f49654503fdc0b28835";
+    },
+  };
+}
+
 return boot
--- demo/live-bootstrap/steps/automake-1.7/pass1.sh
+++ demo/live-bootstrap/steps/automake-1.7/pass1.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 src_prepare() {
-    rm -- configure Makefile.in */Makefile.in */*/Makefile.in aclocal.m4 automake.info*
+    rm -- configure Makefile.in lib/Automake/Makefile.in lib/Makefile.in lib/am/Makefile.in m4/Makefile.in tests/Makefile.in aclocal.m4 automake.info*
     sed -i -e 's/2.54/2.53/' -e '/AC_PROG_EGREP/d' -e '/AC_PROG_FGREP/d' configure.in
     aclocal-1.6
     autoconf-2.53
@@ -20,5 +20,5 @@ src_compile() {
 
 src_install() {
     make install MAKEINFO=true DESTDIR="${DESTDIR}"
-    rm "${DESTDIR}/usr/bin/automake" "${DESTDIR}/usr/bin/aclocal"
+    rm "${DESTDIR}${PREFIX}/bin/automake" "${DESTDIR}${PREFIX}/bin/aclocal"
 }
