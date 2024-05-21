a7790854087cf915b3a3c7642da6d6156b3492b4
Ross Light <ross@zombiezen.com>
2024-05-21 15:35:37 -0700

    Build mpfr

--- demo/bootstrap.lua
+++ demo/bootstrap.lua
@@ -3114,4 +3114,61 @@ boot.autoconf_archive = bashStep {
   };
 }
 
+boot.mpfr = bashStep {
+  pname = "mpfr";
+  version = "4.1.0";
+
+  builder = boot.bash["5.2.15"].."/bin/bash";
+  PATH = mkBinPath {
+    boot.pkg_config,
+    boot.findutils,
+    boot.gcc["4.0.4-pass1"],
+    boot.binutils["2.30"],
+    boot.libtool["2.4.7"],
+    boot.help2man,
+    boot.automake["1.15.1"],
+    boot.autoconf["2.69"],
+    boot.perl["5.6.2"],
+    boot.gawk["3.0.4"],
+    boot.diffutils["2.7"],
+    boot.grep["2.4"],
+    boot.bison["3.4.1"],
+    boot.flex["2.6.4"],
+    boot.m4["1.4.7"],
+    boot.bash["5.2.15"],
+    boot.coreutils["9.4"],
+    boot.sed["4.0.9-pass2"],
+    boot.tar["1.34"],
+    boot.gzip["1.2.4"],
+    boot.bzip2.pass2,
+    boot.xz,
+    boot.patch["2.5.9"],
+    boot.make["4.2.1"],
+  };
+
+  C_INCLUDE_PATH = mkIncludePath {
+    boot.musl["1.2.4-pass2"],
+    boot.gmp,
+  };
+  LIBRARY_PATH = mkLibraryPath {
+    boot.musl["1.2.4-pass2"],
+    boot.gmp,
+  };
+  PKG_CONFIG_PATH = makeSearchPathOutput("out", "lib/pkgconfig", {
+    boot.gmp,
+  });
+  ACLOCAL_PATH = makeSearchPathOutput("out", "share/aclocal", {
+    boot.libtool["2.4.7"],
+    boot.pkg_config,
+    boot.autoconf_archive,
+  });
+
+  tarballs = {
+    fetchGNU {
+      path = "mpfr/mpfr-4.1.0.tar.xz";
+      hash = "sha256:0c98a3f1732ff6ca4ea690552079da9c597872d30e96ec28414ee23c95558a7f";
+    },
+  };
+}
+
 return boot
