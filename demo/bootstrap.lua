274120d84c02e3f77be4ee7536393b4165c40104
Ross Light <ross@zombiezen.com>
2024-05-21 07:45:36 -0700

    Build musl 1.2.4 pass2

--- demo/bootstrap.lua
+++ demo/bootstrap.lua
@@ -2424,4 +2424,42 @@ boot.gcc["4.0.4-pass2"] = bashStep {
   };
 }
 
+boot.musl["1.2.4-pass2"] = bashStep {
+  pname = "musl";
+  version = "1.2.4";
+  revision = 1;
+
+  PATH = mkBinPath {
+    boot.gcc["4.0.4-pass2"],
+    boot.binutils["2.30"],
+    boot.libtool["2.2.4"],
+    boot.perl["5.6.2"],
+    boot.gawk["3.0.4"],
+    boot.diffutils["2.7"],
+    boot.grep["2.4"],
+    boot.bison["3.4.1"],
+    boot.flex["2.6.4"],
+    boot.m4["1.4.7"],
+    boot.bash["2.05b-pass1"],
+    boot.coreutils["6.10"],
+    boot.coreutils["5.0-pass2"],
+    boot.sed["4.0.9-pass2"],
+    boot.tar["1.12"],
+    boot.gzip["1.2.4"],
+    boot.bzip2.pass2,
+    boot.patch["2.5.9"],
+    boot.make["3.82-pass1"],
+    stage0.stage0,
+  };
+
+  C_INCLUDE_PATH = mkIncludePath {
+    boot.musl["1.2.4-pass1"],
+  };
+  LIBRARY_PATH = mkLibraryPath {
+    boot.musl["1.2.4-pass1"],
+  };
+
+  tarballs = { musl_1_2_4_tarball };
+}
+
 return boot
