50270c92892561d1404501bdeff3d7fea3f2ac91
Ross Light <ross@zombiezen.com>
2024-05-23 20:59:31 -0700

    Build dist

--- demo/bootstrap.lua
+++ demo/bootstrap.lua
@@ -3447,7 +3447,56 @@ local perl_5_10_1 = bashStep {
   };
 }
 
--- TODO(soon): Remove.
-boot.perl["5.10.1"] = perl_5_10_1
+boot.dist = bashStep {
+  pname = "dist";
+  version = "3.5-236";
+
+  builder = boot.bash["5.2.15"].."/bin/bash";
+  PATH = mkBinPath {
+    boot.pkg_config,
+    boot.findutils,
+    boot.gcc["4.0.4-pass1"],
+    boot.binutils["2.30"],
+    boot.libtool["2.4.7"],
+    boot.help2man,
+    boot.automake["1.15.1"],
+    boot.autoconf["2.69"],
+    perl_5_10_1,
+    boot.gawk["3.0.4"],
+    boot.diffutils["2.7"],
+    boot.grep["2.4"],
+    boot.bison["2.3"], -- Intentionally using an old version.
+    boot.flex["2.6.4"],
+    boot.m4["1.4.7"],
+    boot.bash["5.2.15"],
+    boot.coreutils["9.4"],
+    boot.sed["4.0.9-pass2"],
+    boot.tar["1.34"],
+    boot.gzip["1.2.4"],
+    boot.bzip2.pass2,
+    boot.xz,
+    boot.patch["2.5.9"],
+    boot.make["4.2.1"],
+  };
+
+  C_INCLUDE_PATH = mkIncludePath {
+    boot.musl["1.2.4-pass2"],
+  };
+  LIBRARY_PATH = mkLibraryPath {
+    boot.musl["1.2.4-pass2"],
+  };
+  ACLOCAL_PATH = makeSearchPathOutput("out", "share/aclocal", {
+    boot.libtool["2.4.7"],
+    boot.pkg_config,
+    boot.autoconf_archive,
+  });
+
+  tarballs = {
+    fetchurl {
+      url = "http://deb.debian.org/debian/pool/main/d/dist/dist_3.5-236.orig.tar.gz";
+      hash = "sha256:05fa4f6ea9f05adf8f577699cb3f5b88b20dfce86b0d0cebbfb072fe5933d38f";
+    },
+  };
+}
 
 return boot
