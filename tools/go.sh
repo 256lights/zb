d97f8d55f2612610c8224522b23cafdbef0fc3f8
Roxy Light <roxy@zombiezen.com>
2024-08-27 09:06:13 -0700

    Disable stderr on `go env`

--- tools/go.sh
+++ tools/go.sh
@@ -7,4 +7,9 @@
 #   "go": "${workspaceFolder}/tools/go.sh"
 # },
 
-DIRENV_LOG_FORMAT='' exec direnv exec "$(dirname "$0")/.." go "$@"
+export DIRENV_LOG_FORMAT=''
+if [ $# -gt 1 ] && [ "$1" = env ]; then
+  # VSCode does not like having stderr output on go env.
+  exec direnv exec "$(dirname "$0")/.." go "$@" 2> /dev/null
+fi
+exec direnv exec "$(dirname "$0")/.." go "$@"
