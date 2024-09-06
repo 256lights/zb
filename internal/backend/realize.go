33dc8799439c24ec0898f22a158cf4dce7b9a447
Roxy Light <roxy@zombiezen.com>
2024-09-05 22:14:52 -0700

    Split mutexMap into separate file

--- internal/backend/realize.go
+++ internal/backend/realize.go
@@ -1021,47 +1021,6 @@ func (logger *rpcLogger) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
-// A mutexMap is a map of mutexes.
-// The zero value is an empty map.
-type mutexMap[T comparable] struct {
-	mu sync.Mutex
-	m  map[T]<-chan struct{}
-}
-
-// lock waits until it can either acquire the mutex for k
-// or ctx.Done is closed.
-// If lock acquires the mutex, it returns a function that will unlock the mutex and a nil error.
-// Otherwise, lock returns a nil unlock function and the result of ctx.Err().
-// Until unlock is called, all calls to mm.lock(k) for the same k will block.
-// Multiple goroutines can call lock simultaneously.
-func (mm *mutexMap[T]) lock(ctx context.Context, k T) (unlock func(), err error) {
-	for {
-		mm.mu.Lock()
-		workDone := mm.m[k]
-		if workDone == nil {
-			c := make(chan struct{})
-			if mm.m == nil {
-				mm.m = make(map[T]<-chan struct{})
-			}
-			mm.m[k] = c
-			mm.mu.Unlock()
-			return func() {
-				mm.mu.Lock()
-				delete(mm.m, k)
-				close(c)
-				mm.mu.Unlock()
-			}, nil
-		}
-		mm.mu.Unlock()
-
-		select {
-		case <-workDone:
-		case <-ctx.Done():
-			return nil, ctx.Err()
-		}
-	}
-}
-
 func formatOutputPaths(m map[string]zbstore.Path) string {
 	sb := new(strings.Builder)
 	for i, outputName := range xmaps.SortedKeys(m) {
--- internal/backend/realize_test.go
+++ internal/backend/realize_test.go
@@ -12,7 +12,6 @@ import (
 	"slices"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/google/go-cmp/cmp"
 	"zombiezen.com/go/log/testlog"
@@ -392,58 +391,6 @@ func checkSingleFileOutput(tb testing.TB, wantOutputPath zbstore.Path, wantOutpu
 		tb.Error(err)
 	} else if got := info.Mode(); got&0o111 != 0 {
 		tb.Errorf("%s mode = %v; want non-executable", gotOutputPath, got)
-	}
-}
-
-func TestMutexMap(t *testing.T) {
-	// Prevent this test from blocking for more than 10 seconds.
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	mm := new(mutexMap[int])
-	unlock1, err := mm.lock(ctx, 1)
-	if err != nil {
-		t.Fatal("lock(ctx, 1) on new map failed:", err)
-	}
-
-	// Verify that we can acquire a lock on an independent key.
-	unlock2, err := mm.lock(ctx, 2)
-	if err != nil {
-		t.Fatal("lock(ctx, 2) after lock(ctx, 1) failed:", err)
-	}
-
-	// Verify that attempting a lock on the same key blocks until Done.
-	failFastCtx, cancelFailFast := context.WithTimeout(ctx, 100*time.Millisecond)
-	unlock1b, err := mm.lock(failFastCtx, 1)
-	cancelFailFast()
-	if err == nil {
-		t.Error("lock(ctx, 1) acquired without releasing unlock1")
-		unlock1b()
-	}
-
-	// Verify that unlocking a key allows a subsequent lock to succeed.
-	unlock1()
-	unlock1, err = mm.lock(ctx, 1)
-	if err != nil {
-		t.Fatal("lock(ctx, 1) after unlock1 failed:", err)
-	}
-
-	// Verify that unlocking a key allows a concurrent lock to succeed.
-	lock2Done := make(chan error)
-	go func() {
-		_, err := mm.lock(ctx, 2)
-		lock2Done <- err
-	}()
-	// Wait for a little bit to make it more likely that the other goroutine hit lock(2).
-	timer := time.NewTimer(10 * time.Millisecond)
-	select {
-	case <-timer.C:
-	case <-ctx.Done():
-		timer.Stop()
-	}
-	unlock2()
-	if err := <-lock2Done; err != nil {
-		t.Error("lock(ctx, 2) with concurrent unlock2 failed:", err)
 	}
 }
 
