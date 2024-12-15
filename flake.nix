45df8cff3393f1ac4a2df27ce4f6aa6dc20edad7
Roxy Light <roxy@256lights.llc>
2024-12-14 18:37:54 -0800

    Update to Go 1.23.3

--- flake.nix
+++ flake.nix
@@ -4,12 +4,11 @@
     flake-utils.url = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ self.overlays.default ];
         };
       in
       {
@@ -17,6 +16,7 @@
           packages = [
             pkgs.delve
             pkgs.go_1_23
+            pkgs.gotools  # stringer, etc.
             (pkgs.gopls.override {
               buildGoModule = pkgs.buildGo123Module;
             })
@@ -25,15 +25,5 @@
           hardeningDisable = [ "fortify" ];
         };
       }
-    ) // {
-      overlays.default = final: prev: {
-        go_1_23 = prev.go_1_23.overrideAttrs {
-          version = "1.23.2";
-          src = prev.fetchurl {
-            url = "https://go.dev/dl/go1.23.2.src.tar.gz";
-            hash = "sha256-NpMBYqk99BfZC9IsbhTa/0cFuqwrAkGO3aZxzfqc0H8=";
-          };
-        };
-      };
-    };
+    );
 }
