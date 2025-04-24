b6cbe205592a1a75927091a44cd01b0458fbb270
Roxy Light <roxy@256lights.llc>
2025-04-23 20:51:08 -0700

    Build GNU awk 5.3.2
    Updates #96

---
+++ lib/packages/gawk/gawk.lua
@@ -0,0 +1,46 @@
+-- Copyright 2025 The zb Authors
+-- SPDX-License-Identifier: MIT
+
+local bootstrap <const> = import "../../bootstrap/seeds.lua"
+local fetchGNU <const> = import "../../fetchgnu.lua"
+local tables <const> = import "../../tables.lua"
+
+local module <const> = {}
+
+local tarballArgs <const> = {
+  ["5.3.2"] = {
+    path = "gawk/gawk-5.3.2.tar.xz";
+    hash = "sha256:f8c3486509de705192138b00ef2c00bbbdd0e84c30d5c07d23fc73a9dc4cc9cc";
+  };
+}
+
+module.tarballs = tables.lazyMap(fetchGNU, tarballArgs)
+
+---@param makeDerivation function
+---@param system string
+---@param version string
+---@return derivation
+function module.new(makeDerivation, system, version)
+  local src = module.tarballs[version]
+  if not src then
+    error("gawk.new: unsupported version "..version)
+  end
+  return makeDerivation {
+    pname = "gawk";
+    version = version;
+    system = system;
+    src = src;
+  }
+end
+
+for system in pairs(bootstrap) do
+  local system <const> = system
+  module[system] = tables.lazyModule {
+    stdenv = function()
+      local stdenv <const> = import "../../stdenv/stdenv.lua"
+      return module.new(stdenv.makeBootstrapDerivation, system, "5.3.2")
+    end;
+  }
+end
+
+return module
