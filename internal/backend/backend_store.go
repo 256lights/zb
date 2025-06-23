256lights/zb

commit 89ba1db637398a44b50d6b387d30fffce4a42a0f

Respect reference order during store deletion
The references map was not being populated,
so the deletion order was random.

Fixes #135

internal/backend/backend_store.go
@@ -282,22 +282,31 @@ func pathInfo(conn *sqlite.Conn, path zbstore.Path) (_ *ObjectInfo, err error) {
 		return nil, fmt.Errorf("path info for %s: %w", path, errObjectNotExist)
 	}
 
-	err = sqlitex.ExecuteTransientFS(conn, sqlFiles(), "references.sql", &sqlitex.ExecOptions{
+	info.References, err = listReferences(conn, path)
+	if err != nil {
+		return nil, fmt.Errorf("path info for %s: %v", path, err)
+	}
+
+	return info, nil
+}
+
+func listReferences(conn *sqlite.Conn, path zbstore.Path) (sets.Sorted[zbstore.Path], error) {
+	var references sets.Sorted[zbstore.Path]
+	err := sqlitex.ExecuteTransientFS(conn, sqlFiles(), "references.sql", &sqlitex.ExecOptions{
 		Named: map[string]any{":path": string(path)},
 		ResultFunc: func(stmt *sqlite.Stmt) error {
 			ref, err := zbstore.ParsePath(stmt.GetText("path"))
 			if err != nil {
 				return err
 			}
-			info.References.Add(ref)
+			references.Add(ref)
 			return nil
 		},
 	})
 	if err != nil {
-		return nil, fmt.Errorf("path info for %s: references: %v", path, err)
+		return sets.Sorted[zbstore.Path]{}, fmt.Errorf("references for %s: %v", path, err)
 	}
-
-	return info, nil
+	return references, nil
 }
 
 var errObjectNotExist = errors.New("object not in store")

internal/backend/backend.go
@@ -731,15 +731,6 @@ func (s *Server) delete(ctx context.Context, paths sets.Set[zbstore.Path], recur
 			if !recursive {
 				return fmt.Errorf("store objects have references")
 			}
-			for path := range paths.All() {
-				insertStmt.SetText(":path", string(path))
-				if _, err := insertStmt.Step(); err != nil {
-					return fmt.Errorf("%s: %v", path, err)
-				}
-				if err := insertStmt.Reset(); err != nil {
-					return fmt.Errorf("%s: %v", path, err)
-				}
-			}
 		}
 		if err := sqlitex.ExecuteScriptFS(conn, sqlFiles(), "delete/drop_target_table.sql", nil); err != nil {
 			return err
@@ -749,6 +740,13 @@ func (s *Server) delete(ctx context.Context, paths sets.Set[zbstore.Path], recur
 		allPaths = make([]zbstore.Path, 0, paths.Len()+reverseDeps.Len())
 		allPaths = slices.AppendSeq(allPaths, xiter.Chain(paths.All(), reverseDeps.All()))
 		references := make(map[zbstore.Path]sets.Sorted[zbstore.Path], len(allPaths))
+		for _, path := range allPaths {
+			var err error
+			references[path], err = listReferences(conn, path)
+			if err != nil {
+				return err
+			}
+		}
 		err = sortByReferences(
 			allPaths,
 			func(p zbstore.Path) zbstore.Path { return p },

commit 556827fa3f1dda1aac06908abe63e4e2b4f71b89

Fix breakages introduced by 89ba1db637398a44b50d6b387d30fffce4a42a0f

internal/backend/backend_store.go
@@ -405,7 +405,7 @@ func sortByReferences[T any](slice []T, pathFunc func(T) zbstore.Path, refsFunc 
 				if ref == p {
 					continue
 				}
-				if isSorted, known := sortStatus[ref]; !isSorted && (known || ignoreUnknown) {
+				if isSorted, known := sortStatus[ref]; !isSorted && (known || !ignoreUnknown) {
 					return false
 				}
 			}

internal/backend/export.go
@@ -167,9 +167,6 @@ func (s *Server) findExportClosure(ctx context.Context, paths []zbstore.Path) ([
 		})
 	}
 	for _, path := range paths {
-		sortEnd := len(result)
-
-		// Gather closure.
 		var infoError error
 		err := closurePaths(conn, pathAndEquivalenceClass{path: path}, func(pe pathAndEquivalenceClass) bool {
 			if hasPath(result, pe.path) {
@@ -189,17 +186,17 @@ func (s *Server) findExportClosure(ctx context.Context, paths []zbstore.Path) ([
 		if err != nil {
 			return nil, err
 		}
+	}
 
-		// Topologically sort new closure.
-		err = sortByReferences(
-			result[sortEnd:],
-			func(t *zbstore.ExportTrailer) zbstore.Path { return t.StorePath },
-			func(t *zbstore.ExportTrailer) sets.Sorted[zbstore.Path] { return t.References },
-			false,
-		)
-		if err != nil {
-			return nil, fmt.Errorf("closure of %s missing referenced objects", path)
-		}
+	// Topologically sort new closure.
+	err = sortByReferences(
+		result,
+		func(t *zbstore.ExportTrailer) zbstore.Path { return t.StorePath },
+		func(t *zbstore.ExportTrailer) sets.Sorted[zbstore.Path] { return t.References },
+		false,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("closure of %s missing referenced objects", paths)
 	}
 
 	return result, nil