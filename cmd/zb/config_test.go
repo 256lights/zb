d751912f47d1e79a70fc0b9db24bec4a643d31da
Roxy Light <roxy@256lights.llc>
2025-11-03 11:50:18 -0800

    Turn config tests into table test

--- cmd/zb/config_test.go
+++ cmd/zb/config_test.go
@@ -4,11 +4,13 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"slices"
 	"testing"
 
-	"zb.256lights.llc/pkg/zbstore"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestDefaultGlobalConfig(t *testing.T) {
@@ -22,32 +24,44 @@ func TestDefaultGlobalConfig(t *testing.T) {
 }
 
 func TestGlobalConfigMergeFiles(t *testing.T) {
-	dir := t.TempDir()
-	var paths [2]string
-	paths[0] = filepath.Join(dir, "config1.jwcc")
-	if err := os.WriteFile(paths[0], []byte(`{"debug": true, "storeDirectory": "/foo"}`+"\n"), 0o666); err != nil {
-		t.Fatal(err)
-	}
-	paths[1] = filepath.Join(dir, "config2.jwcc")
-	if err := os.WriteFile(paths[1], []byte(`{"storeDirectory": "/bar"}`+"\n"), 0o666); err != nil {
-		t.Fatal(err)
+	tests := []struct {
+		name  string
+		files []string
+		want  globalConfig
+	}{
+		{
+			name: "MergeScalar",
+			files: []string{
+				`{"debug": true, "storeDirectory": "/foo"}` + "\n",
+				`{"storeDirectory": "/bar"}` + "\n",
+			},
+			want: globalConfig{
+				Debug:     true,
+				Directory: "/bar",
+			},
+		},
 	}
 
-	g := new(globalConfig)
-	err := g.mergeFiles(func(yield func(string) bool) {
-		for _, path := range paths {
-			if !yield(path) {
-				return
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			dir := t.TempDir()
+			paths := make([]string, len(test.files))
+			for i, content := range test.files {
+				path := filepath.Join(dir, fmt.Sprintf("config%d.jwcc", i+1))
+				if err := os.WriteFile(path, []byte(content), 0o666); err != nil {
+					t.Fatal(err)
+				}
+				paths[i] = path
 			}
-		}
-	})
-	if err != nil {
-		t.Error("mergeFiles:", err)
-	}
-	if !g.Debug {
-		t.Error("g.Debug = false; want true (config1.jwcc ignored)")
-	}
-	if got, want := g.Directory, zbstore.Directory("/bar"); got != want {
-		t.Errorf("g.Directory = %q; want %q", got, want)
+
+			got := new(globalConfig)
+			err := got.mergeFiles(slices.Values(paths))
+			if err != nil {
+				t.Error("mergeFiles:", err)
+			}
+			if diff := cmp.Diff(&test.want, got, cmp.AllowUnexported(stringAllowList{})); diff != "" {
+				t.Errorf("-want +got:\n%s", diff)
+			}
+		})
 	}
 }
