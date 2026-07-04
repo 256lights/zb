e68abf3f48c1bb28fd130545cd0f792f9cb58462
Roxy Light <roxy@256lights.llc>
2026-07-04 13:48:39 -0700

    Extract generic HTTP helpers in `internal/remotestore` to a new file

--- internal/remotestore/httpstore.go
+++ internal/remotestore/httpstore.go
@@ -4,14 +4,11 @@
 package remotestore
 
 import (
-	"bytes"
 	"context"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
-	"strconv"
 
 	jsonv2 "github.com/go-json-experiment/json"
 	"zb.256lights.llc/pkg/internal/fileurl"
@@ -220,55 +217,6 @@ func (s *HTTPStore) addRealizations(ctx context.Context, dst *zbstore.Realizatio
 	return nil
 }
 
-func fetch(ctx context.Context, client *http.Client, u *url.URL, accept string) ([]byte, error) {
-	req := (&http.Request{
-		Method: http.MethodGet,
-		URL:    u,
-		Header: http.Header{
-			"Accept":          {accept},
-			"Accept-Encoding": {httpencoding.Accept},
-			"User-Agent":      {useragent.String},
-		},
-	}).WithContext(ctx)
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("fetch %v: %v", u.Redacted(), err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("fetch %v: %w", u.Redacted(), &httpError{
-			statusCode: resp.StatusCode,
-			status:     resp.Status,
-		})
-	}
-	const mebibyte = 1 << 20
-	const maxSize = 4 * mebibyte
-	if resp.ContentLength > maxSize {
-		return nil, fmt.Errorf("fetch %v: response too large (%.1f MiB)", u.Redacted(), float64(resp.ContentLength)/mebibyte)
-	}
-	data, err := io.ReadAll(io.LimitReader(resp.Body, maxSize))
-	if err != nil {
-		return nil, fmt.Errorf("fetch %v: %v", u.Redacted(), err)
-	}
-	if resp.ContentLength == -1 && len(data) == maxSize {
-		if n, _ := resp.Body.Read(make([]byte, 1)); n > 0 {
-			return nil, fmt.Errorf("fetch %v: response too large", u.Redacted())
-		}
-	}
-	if e := resp.Header.Get("Content-Encoding"); e != "" {
-		dec, err := httpencoding.Decode(bytes.NewReader(data), e)
-		if err != nil {
-			return nil, fmt.Errorf("fetch %v: %v", u.Redacted(), err)
-		}
-		defer dec.Close()
-		data, err = io.ReadAll(dec)
-		if err != nil {
-			return nil, fmt.Errorf("fetch %v: %v", u.Redacted(), err)
-		}
-	}
-	return data, nil
-}
-
 // httpObject is the implementation of [zbstore.Object] for [HTTPStore].
 type httpObject struct {
 	client *http.Client
@@ -332,31 +280,4 @@ func resolveReference(baseURL, ref *url.URL) (*url.URL, error) {
 		return nil, fmt.Errorf("link to %s not permitted from %s", ref.Redacted(), baseURL.Redacted())
 	}
 	return targetURL, nil
-}
-
-type httpError struct {
-	statusCode int
-	status     string
-}
-
-func (e *httpError) Error() string {
-	status := e.status
-	if status == "" {
-		status = http.StatusText(e.statusCode)
-		if status == "" {
-			status = strconv.Itoa(e.statusCode)
-		}
-	}
-	return "http " + status
-}
-
-func errorStatusCode(err error) (statusCode int, ok bool) {
-	if err == nil {
-		return http.StatusOK, false
-	}
-	var h *httpError
-	if !errors.As(err, &h) {
-		return http.StatusInternalServerError, false
-	}
-	return h.statusCode, true
 }
