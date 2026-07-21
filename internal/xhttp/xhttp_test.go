ebd209501bd00e5f6a1f2444925c5873c70e0016
Roxy Light <roxy@256lights.llc>
2026-07-21 07:14:50 -0700

    Ignore empty elements in `xhttp.SplitList`
    Part of the [list recipient requirements][RFC 9110 Section 5.6.1.2].
    
    [RFC 9110 Section 5.6.1.2]: https://datatracker.ietf.org/doc/html/rfc9110#section-5.6.1.2

--- internal/xhttp/xhttp_test.go
+++ internal/xhttp/xhttp_test.go
@@ -4,9 +4,38 @@
 package xhttp
 
 import (
+	"slices"
 	"strings"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
+
+func TestSplitList(t *testing.T) {
+	tests := []struct {
+		value string
+		want  []string
+	}{
+		{"", nil},
+		{",", nil},
+		{",   ,", nil},
+		{"foo,bar", []string{"foo", "bar"}},
+		{"foo, bar", []string{"foo", "bar"}},
+		{"foo ,bar,", []string{"foo", "bar"}},
+		{"  foo \t , bar \t ", []string{"foo", "bar"}},
+		{"foo , ,bar,charlie", []string{"foo", "bar", "charlie"}},
+		{`no-cache="X-Foo,\"X-Bar",max-age=5`, []string{`no-cache="X-Foo,\"X-Bar"`, "max-age=5"}},
+		{`no-cache="\`, []string{`no-cache="\`}},
+	}
+
+	for _, test := range tests {
+		got := slices.Collect(SplitList(test.value))
+		if diff := cmp.Diff(test.want, got, cmpopts.EquateEmpty()); diff != "" {
+			t.Errorf("SplitList(%q) (-want +got):\n%s", test.value, diff)
+		}
+	}
+}
 
 func TestIsTokenChar(t *testing.T) {
 	sb := new(strings.Builder)
