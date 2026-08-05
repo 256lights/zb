67ebf7fc7474987fd33d310157f14993113e3d15
Roxy Light <roxy@256lights.llc>
2026-08-04 17:10:21 -0700

    Bump version to 0.2.0-beta3 (#310)

--- build.lua
+++ build.lua
@@ -95,7 +95,7 @@ function module.new(args)
   end
   return args.makeDerivation {
     pname = "zb";
-    version = "0.2.0-beta2";
+    version = "0.2.0-beta3";
     src = module.src;
     buildSystem = args.buildSystem;
 
