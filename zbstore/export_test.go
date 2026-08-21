5161ded02578a4fb67c01b54c48acd71bcdf03f4
Roxy Light <roxy@256lights.llc>
2026-08-21 00:14:45 -0700

    Implement `zbstore.Importer` for `*zbstore.ExportWriter` (#357)
    Updates #345

--- zbstore/export_test.go
+++ zbstore/export_test.go
@@ -6,10 +6,13 @@ package zbstore
 import (
 	"bytes"
 	"errors"
+	"io"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
+	"zb.256lights.llc/pkg/internal/xio"
 	"zb.256lights.llc/pkg/sets"
 	"zombiezen.com/go/nix/nar"
 )
@@ -135,6 +138,132 @@ func TestExportWriter(t *testing.T) {
 		}
 
 		verifyExport(t, got.Bytes())
+	})
+
+	t.Run("StoreImport/SingleObject", func(t *testing.T) {
+		const file1Path Path = "/opt/zb/store/mv4z5c5znjdnc40fvqfl1qknszgbdyxd-hello.txt"
+		const file2Content = "I have a ref!\n" + string(file1Path) + "\n"
+		helloExport, err := os.ReadFile(filepath.Join("testdata", "TestExportWriter", "SingleObject.out"))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got := new(bytes.Buffer)
+		ew := NewExportWriter(got)
+
+		if err := ew.StoreImport(t.Context(), bytes.NewReader(helloExport)); err != nil {
+			t.Error("StoreImport:", err)
+		}
+
+		nw := nar.NewWriter(ew)
+		err = nw.WriteHeader(&nar.Header{
+			Size: int64(len(file2Content)),
+		})
+		if err != nil {
+			t.Error("write NAR 2 header:", err)
+		}
+		if _, err := nw.Write([]byte(file2Content)); err != nil {
+			t.Error("write NAR 2 file content:", err)
+		}
+		if err := nw.Close(); err != nil {
+			t.Error("close NAR 2:", err)
+		}
+
+		err = ew.Trailer(&ExportTrailer{
+			StorePath:      "/opt/zb/store/cpkdqrc1w4kaklx7881c8c4lw46dzrf7-ref.txt",
+			References:     *sets.NewSorted(file1Path),
+			ContentAddress: mustParseContentAddress(t, "fixed:r:sha256:0zxvgbzvvxrxx23h50afmv5rzl5xgkbh20rss0xd5319wrd8511y"),
+		})
+		if err != nil {
+			t.Error("write trailer:", err)
+		}
+
+		if err := ew.Close(); err != nil {
+			t.Error("Close:", err)
+		}
+
+		verifyExport(t, got.Bytes())
+	})
+
+	t.Run("StoreImport/ImmediateEOF", func(t *testing.T) {
+		const fileContent = "Hello, World!\n"
+
+		got := new(bytes.Buffer)
+		ew := NewExportWriter(got)
+		if err := ew.StoreImport(t.Context(), strings.NewReader(exportEOFMarker)); err != nil {
+			t.Error("StoreImport:", err)
+		}
+		nw := nar.NewWriter(ew)
+		err := nw.WriteHeader(&nar.Header{
+			Size: int64(len(fileContent)),
+		})
+		if err != nil {
+			t.Error("write NAR header:", err)
+		}
+		if _, err := nw.Write([]byte(fileContent)); err != nil {
+			t.Error("write NAR file content:", err)
+		}
+		if err := nw.Close(); err != nil {
+			t.Error("close NAR:", err)
+		}
+		err = ew.Trailer(&ExportTrailer{
+			StorePath:      "/opt/zb/store/mv4z5c5znjdnc40fvqfl1qknszgbdyxd-hello.txt",
+			ContentAddress: mustParseContentAddress(t, "fixed:r:sha256:1qicirpsz48j7a2r5h9lj04kipdyvxanwglv9ymfq0qsv7isywdf"),
+		})
+		if err != nil {
+			t.Error("write trailer:", err)
+		}
+		if err := ew.Close(); err != nil {
+			t.Error("Close:", err)
+		}
+
+		verifyExport(t, got.Bytes())
+	})
+
+	t.Run("StoreImport/Empty", func(t *testing.T) {
+		ew := NewExportWriter(io.Discard)
+		if err := ew.StoreImport(t.Context(), xio.Null()); err == nil {
+			t.Error("StoreImport did not return error")
+		} else {
+			t.Log("StoreImport:", err)
+		}
+	})
+
+	t.Run("StoreImport/ImmediateEOFTrailingData", func(t *testing.T) {
+		ew := NewExportWriter(io.Discard)
+		if err := ew.StoreImport(t.Context(), strings.NewReader(exportEOFMarker+"\x00")); err == nil {
+			t.Error("StoreImport did not return error")
+		} else {
+			t.Log("StoreImport:", err)
+		}
+	})
+
+	t.Run("StoreImport/InvalidMagic", func(t *testing.T) {
+		badExport := strings.Repeat("\xba", len(exportObjectMarker))
+		ew := NewExportWriter(io.Discard)
+		if err := ew.StoreImport(t.Context(), strings.NewReader(badExport)); err == nil {
+			t.Error("StoreImport did not return error")
+		} else {
+			t.Log("StoreImport:", err)
+		}
+	})
+
+	t.Run("StoreImport/InvalidEOFMagic", func(t *testing.T) {
+		badExport, err := os.ReadFile(filepath.Join("testdata", "TestExportWriter", "SingleObject.out"))
+		if err != nil {
+			t.Fatal(err)
+		}
+		eofMarker := badExport[len(badExport)-len(exportEOFMarker):]
+		for i := range eofMarker {
+			eofMarker[i] = '\xba'
+		}
+
+		ew := NewExportWriter(io.Discard)
+		if err := ew.StoreImport(t.Context(), bytes.NewReader(badExport)); err == nil {
+			t.Error("StoreImport did not return error")
+		} else {
+			t.Log("StoreImport:", err)
+		}
 	})
 }
 
